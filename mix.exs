31487ed59a1afc648116df2a2442a107b223b394
Joseph DelCioppio <jdelcioppio@mojotech.com>
2019-05-16 08:40:13 -0400

    Update dependencies for Elixir 1.8.1

--- mix.exs
+++ mix.exs
@@ -3,11 +3,11 @@ defmodule ExDash.Mixfile do
 
   def project do
     [app: :ex_dash,
-     version: "0.1.7",
-     elixir: "~> 1.4",
+     version: "0.1.8",
+     elixir: "~> 1.8",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     source_url: "https://github.com/urbint/ex_dash",
+     source_url: "https://github.com/thedelchop/ex_dash",
      deps: deps(),
      package: package(),
      description: description(),
@@ -33,9 +33,9 @@ defmodule ExDash.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.18.3"},
-      {:floki, "~> 0.20.3"},
-      {:mochiweb_html, "~> 2.15"},
+      {:ex_doc, "0.19.2"},
+      {:floki, "~> 0.21.0"},
+      {:mochiweb, "~> 2.18"},
       {:cortex, "~> 0.5.0"}
     ]
   end
@@ -48,9 +48,9 @@ defmodule ExDash.Mixfile do
 
   defp package do
     [
-     maintainers: ["Russell Matney"],
+     maintainers: ["Russell Matney", "Joseph DelCioppio"],
      licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/russmatney/ex_dash"}
+     links: %{"GitHub" => "https://github.com/thedelchop/ex_dash"}
     ]
   end
 end
